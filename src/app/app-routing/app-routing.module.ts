80b509a55fdd90e14ae5748ac1a672aa802e2068
Dibin Joseph <Dibin.Joseph@in.ey.com>
2019-09-24 15:42:33 +0530

    cards

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -10,7 +10,7 @@ import { LoginComponent} from "../login/login.component";
 
 
 const routes: Routes = [
-  { path:'',redirectTo: '/login', pathMatch: 'full' },
+  { path:'',redirectTo: '/dashboard', pathMatch: 'full' },
   { path:"login",component:LoginComponent},
   { path:"home", component: HomeComponent },
   { path:"dashboard", component: DashboardComponent},
